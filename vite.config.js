5715ae624a2637176689ae7af753f33f927873a2
zendrias <zenas@live.com>
2025-08-03 18:31:28 -0400

    remove https

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from "vite";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "frontend-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "frontend.pem")),
-    },
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://localhost:3443",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
